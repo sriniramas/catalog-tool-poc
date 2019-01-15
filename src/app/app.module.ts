436a743979864554de09ac39fb4840537a78aa5f
Asutosh Roy <asutosh.r@hcl.com>
2019-01-15 16:49:32 +0530

    added table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule, TabsModule, AccordionModule, CheckboxModule, RadioModule, TableModule, DropdownModule, LinkModule, ModalModule } from 'carbon-components-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -11,6 +12,7 @@ import { ModelmessageComponent } from './modelmessage/modelmessage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CategoriesListComponent } from './categories-list/categories-list.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
@@ -25,8 +27,8 @@ const routes: Routes = [
     AppComponent, NewMasterCategoryComponent, ModelmessageComponent, NavbarComponent, CategoriesListComponent, LandingPageComponent
   ],
   imports: [
-    BrowserModule, ButtonModule, TabsModule, AccordionModule, CheckboxModule, RadioModule, TableModule, DropdownModule,
-    LinkModule, ModalModule,BrowserAnimationsModule,
+    BrowserModule, FormsModule, ButtonModule, TabsModule, AccordionModule, CheckboxModule, RadioModule, TableModule, DropdownModule,
+    LinkModule, ModalModule, BrowserAnimationsModule,
     RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [],
